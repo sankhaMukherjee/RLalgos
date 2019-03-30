d608f474899922b8cb286cab22c33d966887c704
sankha <sankha.mukherjee@gmail.com>
2019-03-30 08:24:29 +0800

    qNetwork(update): The value for epsilon is now correctly specified

--- src/lib/agents/Agent_DQN.py
+++ src/lib/agents/Agent_DQN.py
@@ -114,7 +114,7 @@ class Agent_DQN:
         try:
             ma = self.maxAction(state)
             ra = self.randomAction(state)
-            p  = np.random.choice([1, 0], size=len(ma), p=[eps, 1-eps]).astype(np.float32)
+            p  = np.random.choice([1, 0], size=len(ma), p=[1-eps, eps]).astype(np.float32)
             p  = torch.as_tensor( p ).to(self.device)
 
             result = ma * p + ra * (1-p)
